26736c0ea8c9f351a48cbb522be6809a267a4ac4
LiuzF-05 <luizfsilvacs@gmail.com>
2026-09-23 00:57:20 -0300

    feat: adds function to yield fetch multiple pages

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
-import json
 import logging
+from collections.abc import Iterator
 from typing import Any, Self
 
 import httpx
@@ -11,7 +11,7 @@ from tenacity import (
 	wait_random_exponential,
 )
 
-from config import ENDPOINT_PATH
+from config import AVAILABLE_COUNTRIES, ENDPOINT_PATH
 
 logger = logging.getLogger(__name__)
 
@@ -74,12 +74,27 @@ class APIClient:
 		response.raise_for_status()
 		return response
 
-	def fetch_page(self, country: str, page: int, page_size: int = 50, ) -> Any:
+	def fetch_page(self, country: str, page: int, page_size: int) -> list[dict]:
 		response = self._get(ENDPOINT_PATH, params={
 			"per_page": page_size, 
 			"page": page, 
 			"by_country": country
 			})
-		response_json = json.loads(response.content)
-		return response_json
+		content_type = response.headers.get("content-type")
+		if content_type != "application/json":
+			logger.error("incorrect content type received: %s", content_type)
+			raise RuntimeError(f"incorrect content type received: {content_type}")
+		return response.json()
 
+	def fetch_all(self, country: str, page_size: int) -> Iterator[list[dict]]:
+		if country not in AVAILABLE_COUNTRIES:
+			logger.error("invalid country inserted: %s", country)
+			raise ValueError(f"invalid country inserted: {country}")
+		page: int = 1
+		while True:
+			response = self.fetch_page(country, page, page_size)
+			yield response
+			if not response or len(response) < page_size:
+				break
+			page += 1
+		
