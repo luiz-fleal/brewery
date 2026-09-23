02337ac25c516d6a2001184f02b24bbb88ed9c5a
LiuzF-05 <luizfsilvacs@gmail.com>
2026-09-23 20:35:23 -0300

    feat: adds new arguments for main.py and implements function to get census data from csv

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+import os
 from collections.abc import Iterator
 
 import polars as pl
@@ -8,18 +9,18 @@ def create_brewery_dataframe(data: Iterator[list[dict]]) -> pl.DataFrame:
         "id": pl.Utf8,
         "name": pl.Utf8,
         "brewery_type": pl.Utf8,
-        "address_1": pl.Utf8,
-        "address_2": pl.Utf8,
-        "address_3": pl.Utf8,
         "city": pl.Utf8,
         "state_province": pl.Utf8,
-        "postal_code": pl.Utf8,
-        "country": pl.Utf8,
-        "longitude": pl.Float64,
-        "latitude": pl.Float64,
         "phone": pl.Utf8,
         "website_url": pl.Utf8,
-        "state": pl.Utf8,
-        "street": pl.Utf8,
     }
-    return pl.from_dicts([d for batch in data for d in batch], schema=schema)
+    return pl.from_dicts([d for batch in data for d in batch], schema=schema).sort(by=["state_province", "city", "name"], descending=[False, False, False])
+
+
+def create_census_dataframe(file_path: str, year: int) -> pl.DataFrame:
+    df = pl.read_csv(
+        os.path.abspath(file_path),
+        encoding="utf-8-lossy",
+        columns=["NAME", "STNAME", f"POPESTIMATE{year}"],
+    )
+    return df
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import logging
 import sys
 
-from analysis import create_brewery_dataframe
+from analysis import create_brewery_dataframe, create_census_dataframe
 from client import APIClient
 from config import BASE_URL
 from fetch_brewery import fetch_all
@@ -15,14 +15,34 @@ def main(args: argparse.Namespace) -> None:
     print(f"starting requests for breweries in the US with page size {args.page_size}")
     with APIClient(BASE_URL) as client:
         data = fetch_all(client=client, page_size=args.page_size)
-        df = create_brewery_dataframe(data)
-    print(df)
+        brewery_df = create_brewery_dataframe(data)
+    census_df = create_census_dataframe("data/population_census.csv", year=args.year)
+    print(census_df)
+    print(brewery_df)
 
 
 def run() -> int:
     # arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-ps", "--page_size", type=int, default=200)
+    parser.add_argument(
+        "--page_size",
+        type=int,
+        default=200,
+        help="number of breweries to fetch per page (default: 200)",
+    )
+    parser.add_argument(
+        "--census_file",
+        type=str,
+        default="data/population_census.csv",
+        help="relative path to population census CSV file (default: data/population_census.csv)",
+    )
+    parser.add_argument(
+        "-y",
+        "--year",
+        type=int,
+        default=2025,
+        help="year for population estimate (default: 2025)",
+    )
     args = parser.parse_args()
 
     # logging and client setup
