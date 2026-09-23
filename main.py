ab7d99452e06c5226f30306d153baef0d909354f
LiuzF-05 <luizfsilvacs@gmail.com>
2026-09-23 01:02:32 -0300

    fix: ruff check fixes and switching to with as

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def main() -> None:
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-c", "--country", type=str)
 	parser.add_argument("-ps", "--page_size", type=int, default=50)
-	args = parser.parse_args()
 
 	# logging and client setup
 	logging.basicConfig(filename="brewery.log", level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-	client = APIClient(BASE_URL)
+	with APIClient(BASE_URL):
+		pass
 
 if __name__ == "__main__":
 	main()
