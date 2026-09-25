f0a1f889d28f9bc19ce4121d06f1287e2a9a96e2
LiuzF-05 <luizfsilvacs@gmail.com>
2026-09-25 13:25:29 -0300

    feat: data cleaning and validation for census csv

--- analysis.py
+++ analysis.py
@@ -1,7 +1,10 @@
+import logging
 import os
 from collections.abc import Iterator
 
 import polars as pl
+
+logger = logging.getLogger(__name__)
 
 
 def create_brewery_dataframe(data: Iterator[list[dict]]) -> pl.DataFrame:
@@ -14,13 +17,54 @@ def create_brewery_dataframe(data: Iterator[list[dict]]) -> pl.DataFrame:
         "phone": pl.Utf8,
         "website_url": pl.Utf8,
     }
-    return pl.from_dicts([d for batch in data for d in batch], schema=schema).sort(by=["state_province", "city", "name"], descending=[False, False, False])
+    df = pl.from_dicts([d for batch in data for d in batch], schema=schema)
+    return df
 
 
 def create_census_dataframe(file_path: str, year: int) -> pl.DataFrame:
-    df = pl.read_csv(
-        os.path.abspath(file_path),
-        encoding="utf-8-lossy",
-        columns=["NAME", "STNAME", f"POPESTIMATE{year}"],
+    df = (
+        pl.read_csv(
+            os.path.abspath(file_path),
+            encoding="utf-8-lossy",
+            columns=["NAME", "STNAME", f"POPESTIMATE{year}"],
+        )
+        .rename(
+            {
+                "NAME": "city",
+                "STNAME": "state_province",
+                f"POPESTIMATE{year}": "population",
+            }
+        )
+        .with_columns(
+            pl.col("city").str.replace(pattern="(city|town|village|County)$", value="")
+        )
     )
-    return df
+
+    # validation with log warnings
+    null_population = df.filter(pl.col("population").is_null())
+    if len(null_population) > 0:
+        logger.warning(
+            f"Found {len(null_population)} rows with null population in census data for year {year}."
+        )
+    nan_population = df.filter(pl.col("population").is_nan())
+    if len(nan_population) > 0:
+        logger.warning(
+            f"Found {len(nan_population)} rows with NaN population in census data for year {year}."
+        )
+    duplicates = df.is_duplicated()
+    if duplicates.sum() > 0:
+        logger.warning(
+            f"Found {duplicates.sum()} duplicate rows in census data for year {year}."
+        )
+    return df.filter(~duplicates)
+
+
+def merge_brewery_and_census_data(
+    brewery_df: pl.DataFrame, census_df: pl.DataFrame
+) -> pl.DataFrame:
+    merged_df = (
+        brewery_df.lazy()
+        .join(census_df.lazy(), on=["city", "state_province"], how="left")
+        .collect()
+    )
+    return merged_df
